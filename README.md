2e3f1ab0f3f306f9afd49b572b79f1fa33e2a3a9
mentaLwz <mentaLwz@outlook.com>
2022-12-04 22:48:11 +0800

    everyday update

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # mentalLearnFullStack
 Learning full stack related
+
+# 规划
+1. 目前目前分为两部分的学习，前端vue，后端gin，首先熟悉框架，然后做一些demo应用练习练习
+2. 然后有问题及时记录一下
+
+
+
+# vue
+- 2022 1204
+	计划跟着教学视频，把脚手架先用起来
+
+# Gin
+- 2022 1204
+	计划跟着教学视频走一遍
+
+
+
+# Every Day Progress
+- 2022 1204
+	把nvim的JavaScript和Go的插件也顺便配一下
+	把在看的视频放进收藏夹，免得忘了，就是vue和gin的
+    - [x] 用vite创建了项目
+    - [x] 学会了使用nerdtree 在另外一个tab打开文件t, 和在tree和文件间切换`ctrl + w h`和`ctrl + w l`，gt和gT在tab间切换
+	next day todo:
+	- [ ] neovim 的包管理插件安装,更多插件
+	- [ ] 初步使用gin和看一下视频，vue也继续看
---
+++ frontend/demo1/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+ <h1>我是傻逼杨银弟</h1>
+<h2> {{name}} {{age}}</h2>
+<button @click="sayHello">fuck you</button>
+
+</template>
+
+
+
+<script>
+  import {h} from 'vue'
+  export default {
+	name: 'App',
+	setup() {
+	  let name = 'zx'
+	  let age = 18
+
+	  // methods
+	  function sayHello() {
+		alert(`my name is ${name}, my age is ${age}`)
+	  }
+
+	  /*return {
+		name,
+		age,
+		sayHello
+	  }
+	  */
+	  return ()=> h('h1', 'Big pig!!!')
+
+
+	}
+
+
+  }
+
+
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
